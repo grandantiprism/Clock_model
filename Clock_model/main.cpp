402e58717eac6af63eb1a729f2b8b28289d3c310
ADE <ackielilac.g1@gmail.com>
2026-01-17 03:50:46 +0900

    Initial Commit

---
+++ Clock_model/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Clock_model
+//
+//  Created by Akihisa Tateishi on 2026/01/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
